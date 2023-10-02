d5378deba5468a6d7ca8a8493ac8c197960012c5
MOHD SAMAD SIDDIQUI <72787049+Mohd-Samad-Siddiqui@users.noreply.github.com>
2023-10-02 07:46:08 +0000

    Variables topics

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -6,10 +6,12 @@ var studentClass = "Beginner";
 studentAddress = "Varanasi";  //We can declare the variable also like this & it is possible because var can be used if there's no keyword for the variable declaration
 
 
+
 console.table([studentId, studentName, studentEmail, studentClass]);
 
 
 /*
+
 var has the functional scope.
 let and const has the block scope.
 In real life we usually don't use var because it can create the confusion between functional and block scope.
@@ -23,7 +25,7 @@ Eg: var name = "Samad";
 -------------------------------
 
 let can be declared with/without initializing the value but const has to be declared with initializing the value.    
-let variable can't be redeclared with the same name in the same block scope.
+let variable can't be redeclared with the same name in the same block scope and same thing is with const variable.
 Eg: let name = "Riya";
     let name = "Raman";
     Here, I'll get the error.
