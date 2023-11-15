d54dfed820681d7999617d7324b18e4cdfda54de
MOHD SAMAD SIDDIQUI <72787049+Mohd-Samad-Siddiqui@users.noreply.github.com>
2023-11-15 19:19:09 +0530

    Update 05_Heap&Stack.js
    Correct the indentation

--- 01_Basics/05_Heap&Stack.js
+++ 01_Basics/05_Heap&Stack.js
@@ -17,25 +17,25 @@ A fixed amount of memory is allocated for static data. This process is known as 
 2. Heap - It is used to store objects and functions in JavaScript. 
 The engine doesn’t allocate a fixed amount of memory. Instead, it allocates more space as required.
 
--------------------------------------------------------------------------------------------------------------------------------
 */
+/*-------------------------------------------------------------------------------------------------------------------------------*/
 
 const employee = { 
     name: 'Rajesh', 
     age: 30, 
     }; 
     
-    const name="Ram"
-    // Allocates memory for object in heap. Values 
-    // in object are primitive, which is why they are stored in stack. 
+const name="Ram"
+// Allocates memory for object in heap. Values  
+// in object are primitive, which is why they are stored in stack. 
     
-    function getname(name) { 
-        return name; 
-    } 
-    // The function return value is given to stack after being evaluated in the heap 
+function getname(name) { 
+    return name; 
+} 
+// The function return value is given to stack after being evaluated in the heap 
     
-    const newEmployee = employee; 
-    // The newEmployee object will be stored in the stack and it will refer to the employee object in heap
+const newEmployee = employee; 
+// The newEmployee object will be stored in the stack and it will refer to the employee object in heap
     
 
 // For better understanding the image link is here: 
