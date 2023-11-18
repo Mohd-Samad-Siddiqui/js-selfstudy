e8a3d39200a42df59a344b90c776f85fc50257a1
MOHD SAMAD SIDDIQUI <72787049+Mohd-Samad-Siddiqui@users.noreply.github.com>
2023-11-18 16:41:24 +0000

    Used Math in Numbers

--- 01_Basics/07_Number_and_Math.js
+++ 01_Basics/07_Number_and_Math.js
@@ -72,9 +72,43 @@ NOTE: The valueOf() method is not directly related to formatting but returns the
       These methods provide flexibility in formatting and presenting numeric values in various ways, 
       making it easier to work with numbers in different contexts.
 
+==============================================================================================================================
+---------------------------MATH-----------------------------
 
+console.log(Math);  //Object [Math] {}
+console.log(Math.abs(-1) +" "+Math.abs(1)); //1 1
+console.log(Math.round(4.6));   //5
+console.log(Math.ceil(4.2));    //5
+console.log(Math.floor(4.6));   //4
+console.log(Math.min(4,6,1,8)); //1
 
+console.log(Math.random());     //0.56789874526753346 (Generates a random floating-point number between 0 (inclusive) and 1 (exclusive))
 
+Now to get the integer value between the given range including min and max values of the range we'll use the given formula.
+    
+    let num = Math.floor(Math.random() * (max - min + 1)) + min;  
+
+    Let's understand with examples:
+
+  a)Example with min = 1 and max = 6:
+    console.log(Math.floor(Math.random() * (6 - 1 + 1)) + 1);
+    Possible output: 1, 2, 3, 4, 5, 6
+
+  b)Example with min = 10 and max = 20:
+    console.log(Math.floor(Math.random() * (20 - 10 + 1)) + 10);
+    Possible output: 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
+
+  c)Example with min = -5 and max = 5:
+    console.log(Math.floor(Math.random() * (5 - (-5) + 1)) + (-5));
+    Possible output: -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5
+
+  d)Example with min = 0 and max = 1:
+    console.log(Math.floor(Math.random() * (1 - 0 + 1)) + 0);
+    Possible output: 0, 1
+
+  e)Example with min = -10 and max = 0:
+    console.log(Math.floor(Math.random() * (0 - (-10) + 1)) + (-10));
+    Possible output: -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0
 
 
 */
