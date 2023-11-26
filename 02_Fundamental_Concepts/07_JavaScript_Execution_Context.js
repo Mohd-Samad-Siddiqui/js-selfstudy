6a5e3f0a47f75eb064ffa06fef90ebbd52aa8473
MOHD SAMAD SIDDIQUI <samad.siddiqui47@gmail.com>
2023-11-26 19:23:55 +0530

    Create 07_JavaScript_Execution_Context.js

---
+++ 02_Fundamental_Concepts/07_JavaScript_Execution_Context.js
@@ -0,0 +1,10 @@
+/*
+
+It's all about understanding the working of javaScript. This lesson included images which isn't possible here for me to bring so I'm putting a video link 
+for better understanding this topic.
+
+YouTube video = (Search) Chai aur Code JavaScript Execute Context 
+or copy this link = https://youtu.be/ByhtOgF6uYM?si=3BTduVMrUMDDfuOD
+
+
+*/
