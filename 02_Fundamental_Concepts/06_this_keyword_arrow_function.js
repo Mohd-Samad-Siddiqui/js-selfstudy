dca2e31e4f64c8e55911dd80e29bf6bebbca5b1a
MOHD SAMAD SIDDIQUI <samad.siddiqui47@gmail.com>
2023-11-26 15:46:27 +0530

    Arrow Function and IIFE
    1. Complete Arrow Functions
    2. IIFE (Named IIFE, Anonymus IIFE, Passing parameter in IIFE)

--- 02_Fundamental_Concepts/06_this_keyword_arrow_function.js
+++ 02_Fundamental_Concepts/06_this_keyword_arrow_function.js
@@ -75,6 +75,84 @@
 
     6. Methods like call(), apply(), and bind(): Read W3Schools doc.
        ----------------------------------------
+==============================================================================================================================================================================================
+
+                  --------------------------------Arrow Function in JavaScript---------------------------------
+                  
+    Arrow functions were introduced in ES6. Arrow functions allow us to write shorter function syntax by removing the function keyword.
+    Syntax: hello = () => {
+              return "Hello World!";
+            }
+
+    Implicit Return (Return by itself): It gets shorter! If the function has only one statement, and the statement returns a value, you can remove the brackets and the return keyword.
+    ---------------------------------
+    Example: hello = () => "Hello World!";
+    Note: This works only if the function has only one statement.
+
+    If you have parameters, you pass them inside the parentheses.
+    Example: productFunc = (a, b) => a * b;
+
+    In fact, if you have only one parameter, you can skip the parentheses as well.
+    Exmaple: hello = name => "Hello " + name;
+          OR hello = name =? ("Hello " + name);     //But if you give curly brackets rather parentheses then you have to use return keyword.
+
+    If you've return an Object with the help of Implicit Return.
+    Example: (Wrong way) objectFunc = () => {name: "Samad"};     //Output: undefined (The curly braces are just for showing it's an object)
+             (Right way) objectFunc = () => ({name: "samad"});   //Output: {name: 'Samad'} (You've to use parentheses for returning the object in case of Implicit Return.
+=============================================================================================================================================================================================
+
+            --------------------------------IIFE (Immediately Invoked Function Expression) in JavaScript---------------------------------
+
+    It is a JavaScript function that is executed as soon as it is defined. The primary purpose of using IIFE is to create a private scope for variables and functions, 
+    preventing them from polluting the global scope.
+    Syntax: (function() {
+                //code inside the IIFE
+            })();
+
+            Breaking the components to understand the syntax:
+            1. First (): The outer parentheses ( ... ) are used to group the function declaration or expression. 
+            2. Function Expression: The function itself is defined inside the parentheses. It can be an anonymous function (a function without a name) or a named 
+                                    function expression.
+            3. Second () for Invoking the function: Immediately after defining the function inside the parentheses, another pair of parentheses () is used to 
+                                                    invoke (call) the function. This is what makes it "immediately invoked."
+            4. (;) Semicolon: At the end it's important. If the first IIFE code isn't having semicolon and there's another IIFE code right after the first IIFE code, 
+                              it could lead to unexpected behavior (it won't let the second IIFE code to run until you give the semicolon to the first IIFE code.)
+
+    Ques. What is Global Pollution?
+    Ans.  Think of the global scope (the entire JavaScript environment) as a big, shared space. If everyone starts putting their stuff (variables and functions) everywhere in that  
+          space without any order, it can lead to chaos. Variables and functions declared without any protection can clash with each other.
+  
+    Without IIFE: Imagine this is the shared space
+    ------------ 
+    Example: var myVar = 'I am global';
+             function myFunction() {
+                // Some code here
+             }
+    If many developers use similar names (like 'myVar' and 'myFunction'), there's a risk of conflicts or unintended interactions.
+            
+    With IIFE: To avoid this issue we use IIFE.
+    ----------
+    Example: (function() {
+              // This is your private space
+                var myVar = 'I am private';   
+                function myFunction() {
+                   // Some code here
+                }
+              })();
+    
+    Named Function IIFE: The naming in the named function expression.  |  Anonymous Function IIFE: There's no naming function expression available.
+    -------------------                                                |  ------------------------
+    Example: (function namedFunction() {                               |  Example:  (function() {
+                // Some code here.                                     |               // code
+             })();                                                     |             })(); 
+
+
+    Passing Argument and Parameter to an IIFE: You can pass the parameter in function(parameter) and to pass an argument you can write in last parenthese (argument);
+    -----------------------------------------
+    Example: This time we'll use arrow function but yeah we can do this with the normal function as well.
+             ((name) => {
+               console.log(`Using parameter in IIFE. Hey I'm ${name}`);
+             })('John');
 
 
 */
